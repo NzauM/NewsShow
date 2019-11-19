c29a1445a38bc4b672ad5908fa70ab3d4797c6c9
NzauM <mercywaenu16@gmail.com>
2019-11-19 12:51:25 +0300

    Add more tests

--- test/test_news.py
+++ test/test_news.py
@@ -14,3 +14,23 @@ class NewsTest(unittest.TestCase):
 
     def test_instance(self):
         self.asserTrue(isinstance(self.new_news,News))
+
+
+    def test_to_check_newssources(self):
+        '''
+        This test will check if news sources are received correctly
+        '''
+        self.assertEquals(self.new_name,'Daily Nation')
+        self.assertEquals(self.new_description,'Nation news')
+        self.assertEquals(self.new_url,'dailynation.co.ke')
+
+    def test_process_results(self):
+        '''
+        This will check if results from the news sources are appended correctly
+        '''
+        self.new_newslist.save_news()
+        self.assertEquals(len(News.news_list),1)
+        
+
+if __name__ == '__main__':
+    unittest.main()
